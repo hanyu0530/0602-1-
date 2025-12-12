1d2ccb88bc14fa67810daeae973b26fb45f8ef1c
hanyu0530 <wuh8947@gmail.com>
2025-12-12 22:16:11 +0800

    Update game.js

--- game.js
+++ game.js
@@ -248,7 +248,7 @@ class Spaceship {
         ctx.font = `bold ${14 + (this.level - 1) * 2}px Arial`;
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-        ctx.fillText('TKU', 0, 0);
+        ctx.fillText('SCU', 0, 0);
 
         // 護盾效果 (大小隨等級變化)
         if (this.shield) {
